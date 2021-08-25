ff39ff7540962d0667bcf446e2f0f0f981ce51d2
Alex Chan <alex.chan@shopify.com>
2021-08-25 18:31:17 -0400

    [Bootstrap] Updated bootstrap.sh to install Neovim nightly

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,12 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 
+# vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Neovim (nightly)
+sudo apt install neovim
 
 if [ $SPIN ]; then
 	echo ""
