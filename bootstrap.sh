8f4fb982e235496e47cf3b7de86e4c010d4150bb
Alex Chan <alex.chan@shopify.com>
2021-08-25 18:23:49 -0400

    [Bootstrap] Updated bootstrap.sh to install vim-plug

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 if [ $SPIN ]; then
 	echo ""
 fi
