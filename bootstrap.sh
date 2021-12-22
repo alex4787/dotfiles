e23f1c507bef3cf26172d16e702b0ba51d7e90f6
Alex Chan <alex.chan@shopify.com>
2021-12-22 11:08:40 -0500

    [Bootstrap] Updated bootstrap.sh (for tmux and iterm)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/iterm ~/iterm
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
