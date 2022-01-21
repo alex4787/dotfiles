8cd00f2ee6aa418f8fae676e634945b27b15751c
Alex Chan <alex.chan@shopify.com>
2022-01-21 00:02:06 -0500

    [Bootstrap] Make oh-my-zsh installation unattended

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ fi
 
 # oh-my-zsh
 export ZSH="$HOME/.oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
